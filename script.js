4682f8c1d89d683cefaaf93304b8da11496c2799
Giulio Starace <giulio.starace@gmail.com>
2018-07-14 14:46:15 +0200

    add canc functionality

--- script.js
+++ script.js
@@ -1,47 +1,28 @@
-
-
-var firstNumber ="";
-var secondNumber = "";
-
+var firstNumber;
+var secondNumber;
 var operation;
-firstNumberEntered = false;
-operationEntered = false;
-secondNumberEntered = false
-
+var firstNumberEntered;
+var operationEntered;
+var secondNumberEntered;
 const screen = document.querySelector("div.screen");
-
+const cancButton = document.querySelector("button#canc");
 const inputButtons = document.querySelectorAll(".notEquals button");
-inputButtons.forEach(button => button.addEventListener('click', function(){
-  if (operationEntered === false){
-    firstNumber += button.textContent;
-    firstNumberEntered = true;
-    screen.textContent = firstNumber;
-  } else {
-    secondNumber += button.textContent;
-    secondNumberEntered = true;
-    screen.textContent += button.textContent;
-  }
-}));
-
 const operatorButtons = document.querySelectorAll(".operators button");
-operatorButtons.forEach(operator => operator.addEventListener('click', function(){
-  if (firstNumberEntered && secondNumberEntered === false){
-    operation = operator.textContent;
-    operationEntered = true;
-    screen.textContent += operation;
-  }
-}));
-
 const equalsButton = document.querySelector("button#equals")
-equalsButton.addEventListener('click', function(){
-  if (firstNumberEntered && secondNumberEntered){
-    screen.textContent = compute(firstNumber, secondNumber, operation)
-  }
-});
 
+function reset(){
+  firstNumber ="";
+  secondNumber = "";
+
+  operation = "";
+  firstNumberEntered = false;
+  operationEntered = false;
+  secondNumberEntered = false;
+  screen.textContent = "";
+}
 function compute(a, b, op){
   if (op == "+"){
-    firstNumber = a + b;
+    firstNumber = parseFloat(a) + parseFloat(b);
   } else if (op == "-") {
     firstNumber = a - b;
   } else if (op == "÷") {
@@ -53,5 +34,36 @@ function compute(a, b, op){
   operation = ""
   secondNumberEntered = false;
   operationEntered = false;
-  return firstNumber
+  return Math.round(firstNumber*1e7)/1e7
 }
+
+reset();
+
+
+cancButton.addEventListener('click', reset);
+
+inputButtons.forEach(button => button.addEventListener('click', function(){
+  if (operationEntered === false){
+    firstNumber += button.textContent;
+    firstNumberEntered = true;
+    screen.textContent = firstNumber;
+  } else {
+    secondNumber += button.textContent;
+    secondNumberEntered = true;
+    screen.textContent = firstNumber + " " + operation + " " + secondNumber;
+  }
+}));
+
+operatorButtons.forEach(operator => operator.addEventListener('click', function(){
+  if (firstNumberEntered && secondNumberEntered === false){
+    operation = operator.textContent;
+    operationEntered = true;
+    screen.textContent = firstNumber + " " + operation ;
+  }
+}));
+
+equalsButton.addEventListener('click', function(){
+  if (firstNumberEntered && secondNumberEntered){
+    screen.textContent = compute(firstNumber, secondNumber, operation)
+  }
+});
